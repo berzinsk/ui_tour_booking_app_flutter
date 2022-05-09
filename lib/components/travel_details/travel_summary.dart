b8387486997d4fd29389c7bb4236d48e615c8a45
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-05-09 14:31:49 +0300

    Add UI For travel summary description

--- lib/components/travel_details/travel_summary.dart
+++ lib/components/travel_details/travel_summary.dart
@@ -9,9 +9,37 @@ class TravelSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        TravelSummaryHeader(),
-        TravelSummaryDetails(),
+      children: [
+        const TravelSummaryHeader(),
+        const TravelSummaryDetails(),
+        Padding(
+          padding: const EdgeInsets.only(top: 26.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(bottom: 12.0),
+                child: Text(
+                  'Description',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'if you wish to experience breezy. beaches, swaying palm trees,'
+                'and quirky beach shacks, make your way to Bali.'
+                'Our Bali tour is dotted with volcanic mountains, iconic rice paddies,'
+                'and vibrant nightlife. Now that you have booked your experiential tour package.',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Color(0xffa3abbf),
+                ),
+              )
+            ],
+          ),
+        )
       ],
     );
   }
